7fb01cb6ffaae5e55bd846f82cc1f29503348394
Jason Gardella <jason.a.gardella@gmail.com>
2015-09-17 08:55:44 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+# epResponsive
+
+The epResponsive plug-in allows columns to be hidden as the window is resized, using metadata-defined column widths for the column definitions.
+
+# Installation
+
+Install using bower
+
+# Usage
+
+Include the feature letter `R` on the dom option. An array of the column definitions should be pushed to the datatables settings object, named `columns`. Each column in the array should
+have a width defined in em units in a string (e.g. "10em").
+
+# GitHub
+
+Submit PRs to the [GitHub repo](https://github.com/eikospartners/datatables-epResponsive).
---
+++ dataTables.epResponsive.js
@@ -0,0 +1,63 @@
+/**------------------------------
+ * epResponsive DataTables Plugin
+ * ------------------------------
+ * Feature Letter: R
+ * 
+ */
+
+(function(window, document, $, undefined) {
+
+var CELL_FONT_SIZE = 14;
+
+$.fn.dataTable.epResponsive = function ( inst ) {
+	var api = new $.fn.dataTable.Api( inst );
+	var settings = api.settings()[0];
+	var columns = settings.oInit.columns;
+	var that = this;
+	
+	// hides columns that cannot fit within the table container's width
+	this._resize_cols = function () {
+		var totalWidth = 0;
+		var tableWidth = $(api.table().container()).width();
+		
+		for(var i = 0; i < columns.length; i++) {
+			// table width is stored as string in em units (e.g. "10em")
+			totalWidth += Number(columns[i].width.match(/(\d+)*/)[0]) * CELL_FONT_SIZE;
+			if(totalWidth < tableWidth) {
+				api.column(i).visible(true);
+			} else {
+				api.column(i).visible(false);
+			}
+		}
+	};
+	
+	// resize columns on window resize
+	$(window).resize(function () {
+		that._resize_cols();
+	});
+	
+	// resize columns on table init
+	api.on("init.dt", function () {
+		that._resize_cols();	
+	});
+	
+	if (settings._responsive ) {
+		throw "Responsive already initialised on table " + settings.nTable.id;
+	}
+	
+	settings._responsive = this;
+};
+
+// Subscribe the feature plug-in to DataTables, ready for use
+$.fn.dataTable.ext.feature.push( {
+	"fnInit": function( settings ) {
+		new $.fn.dataTable.epResponsive( settings );
+	},
+	"cFeature": "R",
+	"sFeature": "epResponsive"
+});
+
+// Create responsive object on API
+$.fn.dataTable.Api.register( 'epResponsive()', function () {} );
+
+})(window, document, jQuery);
