c40c03a289976297d5f2f29330e13ab4cb640cfa
Jason Gardella <jason.a.gardella@gmail.com>
2015-09-21 14:09:56 -0400

    Add fixed column support.
    - Columns can be set a fixed by setting
    property 'fixed' to true in metadata.
    - Fixed columns have priority in being shown.
    - If not all fixed columns can be displayed,
    they will be hidden using default behavior.

--- dataTables.epResponsive.js
+++ dataTables.epResponsive.js
@@ -28,43 +28,78 @@ $.fn.dataTable.epResponsive = function ( inst ) {
 			callback(hiddenColumns);
 		});
 	};
+	
+	// parses width string to number (e.g. "10em" to 140, assuming cell font size of 14)
+	this._parse_width = function (widthString) {
+		return Number(widthString.match(/(\d+)*/)[0]) * CELL_FONT_SIZE;
+	}
+
+	// calculates the initial value for total width.
+	// this is equal to the summation of the widths of fixed columns
+	this._init_total_width = function () {
+		var totalWidth = 0;
+		columns.forEach( function (column) {
+			if(column.fixed) {
+				totalWidth += that._parse_width(column.width);
+			}
+		});
+		return totalWidth;
+	}
+	
+	// sets the visibility of the column in datatables and in the metadata to the given value
+	this._set_column_visibility = function(idx, visible) {
+		api.column(idx).visible(visible);
+		columns[idx].visible = visible;
+	}
 
 	// hides columns that cannot fit within the table container's width
 	this._resize_cols = function () {
-		var totalWidth = 0;
+		var totalWidth = that._init_total_width();
 		var tableWidth = $(api.table().container()).width();
 		var visibilityChanged = false;
 		var hiddenColumns = [];
+		var fixedColumnsOverflow = totalWidth - tableWidth;
+		
 		for(var i = 0; i < columns.length; i++) {
-			// table width is stored as string in em units (e.g. "10em")
-			if(columns[i].width) {
-				totalWidth += Number(columns[i].width.match(/(\d+)*/)[0]) * CELL_FONT_SIZE;
-			} else {
-				totalWidth += CELL_FONT_SIZE;
-				if(!errorLogged) {
-					console.error("Column #" + i + " should have width property. Using default of 1em.");
-					errorLogged = true;
+			if(!columns[i].fixed) {
+				if(columns[i].width) {
+					totalWidth += that._parse_width(columns[i].width);
+				} else {
+					totalWidth += CELL_FONT_SIZE;
+					if(!errorLogged) {
+						console.error("Column #" + i + " should have width property. Using default of 1em.");
+						errorLogged = true;
+					}
 				}
 			}
 			
-			if(totalWidth < tableWidth) {
-				if(api.column(i).visible() !== true) {
+			if(totalWidth < tableWidth || columns[i].fixed) {
+				if(columns[i].visible !== true) {
 					visibilityChanged = true;
 				}
-				api.column(i).visible(true);
-				columns[i].visible = true;
+				that._set_column_visibility(i, true);
 			} else {
-				if(api.column(i).visible() !== false) {
+				if(columns[i].visible !== false) {
 					visibilityChanged = true;
 				}
-				api.column(i).visible(false);
-				columns[i].visible = false;
+				that._set_column_visibility(i, false);
 				hiddenColumns.push(columns[i]);
 			}
 		}
+		
+		// hide fixed columns if needed
+		for(var i = columns.length - 1; i >= 0 && fixedColumnsOverflow > 0; i--) {
+			if(columns[i].fixed) {
+				if(columns[i].visible !== false) {
+					visibilityChanged = true;
+				}
+				that._set_column_visibility(i, false);
+				fixedColumnsOverflow -= that._parse_width(columns[i].width);
+			}
+		}
+		
 		// only call resize callbacks if visibility actually changed
 		if(visibilityChanged) {
-			
 			this._call_resize_callbacks(hiddenColumns);
 		}
 	};
